aeafaf147448e066f4ce617954e4f42eaff68db1
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2021-10-10 16:30:25 +0200

    Service i Contact

---
+++ app.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('.header');
+console.log(header);
+
+window.addEventListener('scroll', function () {
+	console.log(window.scrollY);
+	if (window.scrollY > 135) {
+		header.classList.add('header--sticky');
+	} else {
+		header.classList.remove('header--sticky');
+	}
+});
